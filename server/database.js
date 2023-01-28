83af7bf4a5571d79847d84a936c3d9b4f167ab3f
sabinseredan97 <114243765+sabinseredan97@users.noreply.github.com>
2023-01-28 12:49:03 +0200

    Add files via upload

---
+++ server/database.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+module.exports = db;
---
+++ server/routes/api.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var db = require("../database");
+var router = express.Router();
+
+/* GET users listing. */
+router.post("/paste/insert", (req, res) => {
+  const description = req.body.description;
+  const snippet = req.body.snippet;
+  const query = "INSERT INTO text_snippets (description, snippet) VALUES (?, ?)";
+  db.query(query, [description, snippet], (err, result) => {
+      if (err) {console.log(err)}
+  });
+});
+
+router.get("/paste/search", (req, res) => {
+  const { desc } = req.query;
+  const query = "SELECT id FROM text_snippets WHERE description = ? ORDER BY id DESC LIMIT 1";
+  db.query(query, desc, (err, result) => {
+      res.send(result);
+  });
+});
+
+router.get("/paste/:id", (req, res) => {
+  const id = req.params.id;
+  const query = "SELECT description, snippet FROM text_snippets WHERE id = ?";
+  db.query(query, id, (err, result) => {
+      res.send(result);
+  });
+});
+
+module.exports = router;
