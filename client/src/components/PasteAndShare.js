ef8c6acb0512b7ab5ba64fa904eacefec0467853
sabinseredan97 <114243765+sabinseredan97@users.noreply.github.com>
2023-01-31 15:17:21 +0200

    Add files via upload

---
+++ client/src/components/PasteAndShare.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import Card from "react-bootstrap/Card";
+
+export default function PasteAndShare() {
+  const [pastesList, setPastesList] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await Axios.get("http://localhost:3001/api/paste/list");
+      setPastesList(response.data);
+    }
+    fetchData();
+  }, [])
+
+  return (
+    <>
+      <div>
+        <h1>Paste and Share</h1>
+        <h5>
+          Is a application that lets you share
+          text snippets with other people
+        </h5>
+        <h3>Below is the list of pastes sorted by the most recent ones:</h3>
+      </div>
+      {pastesList.map((paste) => {
+        return (
+          <div key={paste.id}>
+            <form>
+          <Card className="mb-1">
+            <Card.Body className="body">
+              <Card.Title>Description</Card.Title>
+              <Card.Text>
+              <input
+                type="text"
+                className="form-control"
+                value={paste.description}
+                readOnly
+              />
+              </Card.Text>
+              <Card.Title>Paste text</Card.Title>
+              <Card.Text>
+                <textarea
+                  className="form-control"
+                  value={paste.snippet}
+                  readOnly
+                />
+              </Card.Text>
+              <a href={`/paste/${paste.id}`}>Go to this paste</a>
+            </Card.Body>
+          </Card>
+        </form>
+          </div>
+        );
+      })}
+    </>
+  );
+}
